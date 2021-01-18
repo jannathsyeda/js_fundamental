7c5355fe10c98a378a063e684ab9a9b24ae68b82
jannath <jannathsyeda7@gmail.com>
2021-01-18 14:22:22 +0600

    js fubdamental

---
+++ hello.js
@@ -0,0 +1,41 @@
+console.log(9);
+var MyName="John";
+console.log(MyName);
+
+var price=12;
+console.log(typeof price);
+
+var isHot=true;
+console.log(isHot);
+//var ekbar likey onek variable declear hoy,same to same variable name 3ta holey last er ta print kore
+ var name1= "fahi";
+ name2= "tas";
+ name3= "mak";
+
+ console.log(name2);
+
+ //toLowerCase function
+ var nameTOLower="FAHIMA is MY nAme";
+ var makeLower= nameTOLower.toLowerCase("FAHIMA")
+ console.log(makeLower);
+
+//toUpperCase
+var Upper="fahima is my Name .MY ID is someTHINg";
+console.log(Upper.toUpperCase(Upper));
+//indexOf
+var promise="fahima is my Name .MY ID is someTHINg";
+console.log(promise.indexOf('ID'));
+//split ()
+var two="fahima is my Name .MY ID is someTHINg";
+console.log(two.split(' '));
+//convert string in number
+var number1=5;
+var nub2='5';
+nub2=parseFloat(nub2);
+console.log(nub2+number1);
+//float number limit
+var n1=4.9800;
+var n2=8.9599;
+var sum=n1+n2;
+var total=sum.toFixed(4);
+console.log(total);
