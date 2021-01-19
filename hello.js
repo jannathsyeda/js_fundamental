6e19d13feca80ac9aecb09ac775f64b7967da387
jannath <jannathsyeda7@gmail.com>
2021-01-19 13:43:33 +0600

    different methods & functions

--- hello.js
+++ hello.js
@@ -39,3 +39,62 @@ var n2=8.9599;
 var sum=n1+n2;
 var total=sum.toFixed(4);
 console.log(total);
+//white space
+var name1='syeda';
+var name2='fahima';
+var add=name1+' '+name2;
+console.log(add);
+
+//++
+var n1=10;
+n1++;
+console.log(n1);
+// Math absolute round floor ceil random
+var n=2.500;
+var result=Math.ceil(n);
+// var result=Math.round(n);
+// var result=Math.floor(n);
+// var result=Math.abs(n); negative <>positive
+
+console.log(result);
+// Make conditional decision, if-else, comparison
+var mango=10;
+if(mango <=10)
+{
+    console.log("i will buy it");
+}else{
+    console.log("not buying this");
+}
+
+//Multiple conditions, fulfill both conditions, else if
+var p1=false;
+var p2=60;
+if(p1==true && p2==60)
+{
+    console.log("mine");
+}else if(p1==false && p2 >=50)
+{
+    console.log("get this right");
+}else{
+    console.log("nothing");
+}
+//current date
+var date=new Date();
+//var date=new Date(1971-12-16);
+console.log(date);
+//arrayy
+var friendsAge = [15, 16, 19, 20];
+friendsAge[1] = 21;
+var position= friendsAge.indexOf(21);
+console.log(position);
+//Array advanced, push, pop, array length
+var friendsId = [19, 49, 24, 40, 16];
+console.log(friendsId);
+friendsId.push(20);
+friendsId.push(21);
+friendsId.push(22);
+console.log(friendsId);
+friendsId.pop(22);
+console.log(friendsId);
+friendsId.push(21);
+console.log(friendsId);
